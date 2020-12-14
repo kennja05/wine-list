70564eda55c0fa8bfced30704e4a9ee3642a7c6f
kennja05 <jacobkenny05@gmail.com>
2020-12-14 16:35:09 -0500

    add format rating and date functions

--- src/CardFront.js
+++ src/CardFront.js
@@ -1,17 +1,35 @@
 const CardFront = (props) => {
-    console.log(props)
-    const {winery} = props
+    console.log(props.purchase_date)
+    const {winery, varietal, rating, purchase_date} = props
     return(
         <div className='card--front'>
-            <h1>{winery}</h1>
-            <h2>My Cost</h2>
-            {/* TODO: wine-searcher api for average cost */}
-            <h2>AverageCost</h2>
-            <h2>Year</h2>
-            <h2>Rating</h2>
+            <p>{winery}</p>
+            <p>{varietal}</p>
+            <p>{formattedDate(purchase_date)}</p>
+            {/* TODO: wine-searcher api for average cost
+            <p>AverageCost</p> */}
+            <p>{formattedRating(rating)}</p>
         </div>
 
     )
+}
+
+function formattedDate (dateString){
+    const res = dateString.slice(0,10).split('-')
+    res.push(res.shift())
+    return res.join('/')
+}
+
+function formattedRating(int){
+    const star = '⭐'
+    let res = ''
+    if (int === 0){
+        return '0'
+    }
+    for (let i = 0; i < int; i++){
+        res += star
+    }
+    return res
 }
 
 export default CardFront
