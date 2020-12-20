b964e737f25f67a3ab896efead9cb752c9e0da3e
kennja05 <jacobkenny05@gmail.com>
2020-12-20 18:22:25 -0500

    update colors. update card back font size

--- src/CardBack.js
+++ src/CardBack.js
@@ -1,11 +1,13 @@
 const CardBack = (props) => {
-    const {winery, varietal, review, purchase_date} = props
+    const {winery, varietal, review, purchase_date, price} = props
+    console.log(props)
     return(
         <div className='card-back'>
             <div className='card__content'>
                 <p>{winery} | {varietal}</p>
                 <p>{review}</p>
-                {formattedDate(purchase_date)}
+                <p>Purchased On: {formattedDate(purchase_date)}</p>
+                <p>price: ${price}</p>
             </div>
         </div>
     )
